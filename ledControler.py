c2656c0bbadf62f7717c5372945da69a1e3d1148
deshanma <deshanma2010@gmail.com>
2018-05-26 13:29:27 -0700

    add more led

--- ledControler.py
+++ ledControler.py
@@ -8,16 +8,38 @@ GPIO.setwarnings(False)
 GPIO.setup(21,GPIO.OUT)
 GPIO.setup(18,GPIO.OUT)
 GPIO.setup(12,GPIO.OUT)
+GPIO.setup(23,GPIO.OUT)
+GPIO.setup(22,GPIO.OUT)
+count = 0
+GPIO.setup(4,GPIO.OUT)
+GPIO.setup(12,GPIO.OUT)
 
 GPIO.output(18,GPIO.HIGH)
-time.sleep(4)
+time.sleep(0.4)
 GPIO.output(18,GPIO.LOW)
+count2 = count + 1
 
 GPIO.output(21,GPIO.HIGH)
-time.sleep(4)
+time.sleep(0.4)
 GPIO.output(21,GPIO.LOW)
 
+
 GPIO.output(12,GPIO.HIGH)
-time.sleep(1)
+time.sleep(0.4)
 GPIO.output(12,GPIO.LOW)
 
+GPIO.output(22,GPIO.HIGH)
+time.sleep(0.4)
+GPIO.output(22,GPIO.LOW)
+
+GPIO.output(23,GPIO.HIGH)
+time.sleep(0.4)
+GPIO.output(23,GPIO.LOW)
+
+GPIO.output(4,GPIO.HIGH)
+time.sleep(0.4)
+GPIO.output(4,GPIO.LOW)
+
+GPIO.output(12,GPIO.HIGH)
+time.sleep(0.4)
+GPIO.output(12,GPIO.LOW)
